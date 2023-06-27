7b63047dc9a5626b3050373abc0c152a8d80471c
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2023-06-27 16:58:38 +0300

    Create adapter.py

---
+++ structural/adapter.py
@@ -0,0 +1,17 @@
+"""
+    Structural Design Pattern
+    -------------------------
+    Structural design patterns are a category of design patterns in software engineering. These patterns
+    focus on organizing classes and objects to form larger structures and provide solutions for building
+    flexible and efficient software systems. They address the composition of classes and objects to create
+    larger structures while keeping the system flexible, maintainable, and reusable.
+
+    Adapter
+    -------
+    This pattern allows incompatible classes to work together by converting the interface of one class into
+    another expected by the clients. It is useful when you want to use a class that does not have the interface
+    you need. It also allows you to create a reusable class that cooperates with unrelated or unforeseen classes
+    with incompatible interfaces.
+
+    - All descriptions and comments created by ChatGPT and GitHub Copilot
+"""
