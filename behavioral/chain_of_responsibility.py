e8f9c054ace8d7eb498458754c357b984025f1ac
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2023-07-02 20:05:24 +0300

    chain-of-responsbilility-finished

---
+++ behavioral/chain_of_responsibility.py
@@ -0,0 +1,86 @@
+"""
+    Behavioral Design Pattern
+    -------------------------
+    Behavioral design patterns define the communication and interaction between objects or
+    components in a software system, enabling flexible and reusable solutions for managing
+    complex behaviors and interactions. They encompass patterns like Observer, Strategy,
+    Command, and Iterator, providing strategies for organizing and coordinating software
+    component behavior effectively.
+
+    Chain of Responsibility
+    -----------------------
+    Chain of Responsibility is a design pattern that allows objects to handle a request sequentially
+    until it finds a suitable handler. It promotes loose coupling and flexibility in handling requests.
+"""
+from abc import ABC, abstractmethod
+
+class PurchaseRequest:
+    def __init__(self, amount: int):
+        self.amount = amount
+
+class Approver(ABC):
+    def __init__(self):
+        self.successor = None
+
+    @abstractmethod
+    def process_request(self, request: PurchaseRequest):
+        pass
+
+    def set_successor(self, successor):
+        self.successor = successor
+
+class Manager(Approver):
+    name = "Manager"
+
+    def process_request(self, request: PurchaseRequest):
+        if request.amount < 1000:
+            print(f"{self.name} approved request for ${request.amount}")
+        elif self.successor is not None:
+            self.successor.process_request(request)
+
+class Director(Approver):
+    name = "Director"
+
+    def process_request(self, request: PurchaseRequest):
+        if request.amount < 10000:
+            print(f"{self.name} approved request for ${request.amount}")
+        elif self.successor is not None:
+            self.successor.process_request(request)
+
+class CEO(Approver):
+    name = "CEO"
+
+    def process_request(self, request: PurchaseRequest):
+        if request.amount < 25000:
+            print(f"{self.name} approved request for ${request.amount}")
+        elif self.successor is not None:
+            self.successor.process_request(request)
+
+
+if __name__ == "__main__":
+    # Initialize the chain
+    manager = Manager()
+    director = Director()
+    ceo = CEO()
+
+    # Set the successor
+    manager.set_successor(director)
+    director.set_successor(ceo)
+
+    # Create requests
+    request1 = PurchaseRequest(500)
+    request2 = PurchaseRequest(5000)
+    request3 = PurchaseRequest(15000)
+
+    # Process requests
+    manager.process_request(request1)
+    manager.process_request(request2)
+    manager.process_request(request3)
+
+"""
+    Output:
+    -------
+    Manager approved request for $500
+    Director approved request for $5000
+    CEO approved request for $15000
+"""
